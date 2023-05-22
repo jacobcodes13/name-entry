0238519c765d0d78a50ea353a702de8fc62d45d4
Jacob Nevitt <jacobcodes13@gmail.com>
2023-05-22 10:47:17 -0600

    COMPLETED Name Entry

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
+  const [formData, setFormData] = React.useState({
+    name: ""
+  })
+
+  function handleChange(event) {
+    // console.log("changed")
+
+    const {name, value} = event.target
+
+    setFormData(prevFormData => ({
+      ...prevFormData, 
+      [name] : value
+    }))
+  }
+  console.log(formData)
+
+  const [friendList, setFriendList] = React.useState([])
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    // console.log("submitted")
+
+    // need to push the data over to the h1 so it displays the new name thats been entered
+    setFriendList(prevFriendList => [
+      ...prevFriendList, formData.name
+    ])
+  }
+
+  const newFriendList = friendList.map((friend, index) => {
+    return <li key={index}>{friend}</li>
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form" onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder="Name"
+          className="form--name"
+          name="name"
+          onChange={handleChange}
+          value={formData.name}
+        />
+        <h1>{formData.name}</h1>
+        <button className="form--submit">Submit</button>
+        <ol>
+          {newFriendList}
+        </ol>
+      </form>
     </div>
   );
 }
